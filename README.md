b58ddad8b35eba5047143e45417d6c61561dae27
jasonJathanna <nenseyjason03@gmail.com>
2025-10-05 14:29:54 -0400

    adding template, will trim down all no relevent things in the next commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Group 5
+
+## Brainstorm & Feature Ideas
+
+Below are raw notes and feature ideas collected for the project. Keep these as a living list we can refine into tickets.
+
+- Ideas?
+- Register for classes automatically
+- Clone api??? For something?
+- Atrocious loading speeds
+
+Productivity website
+How would our website make life easier?
+
+- Make your calendar automatic?
+	- Scan webpage you're on, propose Title, Start/end time, Date, Location, Description
+	- Able to respond to time conflicts (add functionality later)
+	- Suggestive timeblocking: ask where you can fit an event in blank spaces; look for the most free day
+	- Support repetitive schedules (e.g., create duplicate events like weekly pickleball)
+
+- Scan Google Calendar and Gmail for database
+- AI chatbot (Olama)
+- Button / link to calendar
+
+- Eject HTML to the website it's currently on
+
+- Extension ideas:
+	- Scan email, look for new events; popup: “Would you like to add this to your calendar? Move this around? Propose a time to add to your calendar?”
+	- Voice API integration
+
+- Button: "scan GMails from when?"
+
+- Settings page on web app
+
+- Parse Gmail (hypothetically Outlook)
+- Copy/paste function to parse and create calendar entries
+
+DELEGATE TASKS
+
+Tools: Gemini, Canvas API, voice chat
+
+Not free: Wednesday, Thursday,
+
+---
+
+Notes: keep these items editable — they are meant to be brainstorm notes rather than finalized specs.
---
+++ scripts/content.js
@@ -0,0 +1,140 @@
+// `document.querySelector` may return null if the selector doesn't match anything.
+
+// const text = document.textContent;
+
+let arrayOfNames = ["Kamala","Barack","Chad","Alondra","Chuy","Ashley","Joanna","Joanne","Preston","Michael","Cody","Cole","Cory","Cris","Cruz","Howard","Adolfo","Michele","Dustin","Lidia","Miranda","Scott","Lilia","Linda","Lilly","Keith","Jacob","Dale","Dana","Dani","Dawn","Dave","Jennifer","Kelly","Kelli","Dean","Kenny","Jaime","Jairo","Dina","Kerry","Janet","Jamal","James","Jamie","Wilson","Alejandro","Alejandra","Kevin","Fabiola","Patrick","Dora","Doug","Jared","Drew","Lazaro","Jeanette","Jason","Esperanza","Antonio","Antonia","Earl","Nicole","Cynthia","Eddy","Edna","Chase","Russell","Ingrid","Felicia","Ella","Ada","Elsa","Ale","Amy","Ana","Ann","Art","Ben","Emma","Bob","Roxana","Michelle","Dan","Deb","Dee","Don","Marlon","Eli","Eva","Chloe","Nathaniel","Marsha","Eric","Erik","Erin","Guy","Stanley","Vincent","Roxanne","Ian","Jan","Jay","Jen","Marvin","Jim","Joe","Jon","Joy","Evan","Chris","Kat","Kay","Ken","Kim","Lee","Leo","Martin","Liz","Martha","Luz","Christine","Christina","May","Max","Molly","Meg","Mel","Christian","Mia","Moe","Tanner","Noe","Chuck","Pam","Pat","Raj","Ray","Rey","Rob","Rod","Ron","Roy","Ramon","Sam","Sal","Ralph","Sue","Ted","Tim","Tom","Aurora","Adriana","Randy","Carrie","Lorena","Zoe","Carmen","Efrain","Flor","Carlos","Becky","Becca","Alonso","Cindy","Fran","Fred","Tamara","Bella","Janice","Benny","Katrina","Austin","Tristan","Betsy","Betty","Erica","Erick","Erika","Gabe","Gaby","Gail","Gary","Alfonso","Aaron","Gene","Imelda","Eduardo","Elijah","Suzanne","Gina","Christy","Adrian","Glen","Hunter","Marcia","Cassie","Marcos","Marcus","Mariah","Marina","Marisa","Margie","Gilberto","Raymond","Genesis","Greg","Jessica","Misael","Gabriela","Douglas","Wilfredo","Martina","Consuelo","Veronica","Autumn","Beatriz","Rachel","Moises","Jenna","Jenny","Shane","Donna","Shawn","Shaun","Hope","Sheri","Jerry","Doris","Jesse","Jesus","Sophie","Sophia","Hugo","Jesús","Randall","Warren","Carolina","Caroline","Carolyn","Ethan","Kathleen","Humberto","Clara","Miriam","Dakota","Araceli","Aracely","Olivia","Oliver","Benito","Gregory","Ariana","Yvonne","Leonard","Iris","Irma","Stacey","Francisco","Francisca","Ivan","Adela","Logan","Gregorio","Simon","Alfredo","Reina","Stephanie","Joaquin","Jack","Jade","Jake","Jane","Renee","Jeffery","Jeanne","Dominique","Jean","Jeff","Jenn","Jess","Elizabeth","Uriel","Leonel","Louis","Jill","Reyna","Johnny","Joan","Jodi","Jody","Joel","Joey","John","Jose","Josh","José","Rosemary","Rogelio","Beverly","Juan","Judy","June","Isabel","Billy","Emmanuel","Kara","Kari","Karl","Kate","Katy","Kirk","Louise","Domingo","Dominic","Kris","Tonya","Tomas","Tommy","Lindsey","Lindsay","Kurt","Kristy","Kristi","Krista","Kyle","Damian","Jasmin","Allison","Lalo","Abraham","Steven","Leah","Leon","Lena","Yolanda","Lety","Levi","Lexi","Candace","Dwayne","Patricia","Lily","Lili","Lina","Lisa","Jimmy","Thomas","Claire","Lois","Lola","Ernesto","Lori","Colin","Candice","Javier","Graciela","Brayan","Lucy","Luis","Lulu","Luke","Lupe","Haley","Corey","Meghan","Alexander","Alexandra","Lynn","Brandy","Brandi","Harry","Arlene","Daniel","Orlando","Monica","Marc","Mark","Mari","Mary","Matt","Maya","Alvaro","Isaiah","Isaias","Mike","Mina","Mimi","Whitney","Blake","Mona","Rafael","Selena","Viviana","Juan Carlos","Ricky","Bertha","Phillip","Reginald","Riley","Savannah","Pablo","Nana","Nate","Leonardo","Connie","Connor","Jeffrey","Stella","Neil","Nena","Paige","Tracy","Lupita","Nick","Dulce","Nina","Caitlin","Paola","Noah","Noel","Nora","Tyrone","Paula","Patti","Patty","Alberto","Bianca","Felipe","Charlie","Charles","Aimee","Christopher","Sabrina","Tatiana","Tiffany","Craig","Ignacio","Darren","Maricela","Darryl","Olga","Omar","Brenda","Barbara","Charlene","Sofia","Salvador","Lucas","Lucia","Hailey","Sonia","Sonya","Mauricio","Luisa","Paul","Paty","Yadira","Alyssa","Pepe","Pete","Phil","Leslie","Bobby","Heidi","Phyllis","Gabby","Jazmin","Helen","Nichole","Jacqueline","Spencer","Morgan","Taylor","Henry","Osvaldo","Meredith","Darlene","Gavin","Sergio","Santiago","Leticia","Alexa","Debbie","Courtney","Alina","Alice","Allie","Allen","Allan","Joann","Summer","Raul","Pedro","Rene","Alvin","Peggy","Rick","Rich","Rigo","Rita","Dylan","Penny","Deanna","Brendan","Judith","Kristine","Kristina","Rose","Rosa","Ross","Rosy","Catalina","Josie","Josue","Jorge","Perla","Amber","Peter","Ruby","Rudy","Ruth","Frankie","Robbie","Dolores","Vicente","Ramona","Joyce","Shannon","Isidro","Ryan","Francis","Frances","Gilbert","Sara","Saul","Robert","Bernardo","Sean","Seth","Karina","Shay","Edward","Juliana","Kaitlyn","Nicholas","Brett","Brent","Brian","Rigoberto","Rocio","Stan","Lydia","Robin","Andre","Robyn","Angel","Angie","Magdalena","Arturo","Roger","Vickie","Anita","Jeremiah","Annie","Roman","Bruce","Tara","Bryan","Arthur","Victor","Bryce","Jose Luis","Cameron","Tere","Teri","Melody","Wanda","Rosie","Nathan","Melvin","Heather","Tina","Tito","Annette","Stacy","Todd","Tony","Toni","Tori","Gladys","Steve","Trey","Troy","Wayne","Briana","Elvira","Josefina","Desiree","Abigail","Hilda","Ramiro","Hannah","Theresa","Giovanni","Xavier","Ernest","Madison","Faith","Joseph","Rodney","Yesenia","Rolando","Joshua","Susana","Andrea","Andres","Andrew","Fabian","Jackson","Belinda","Tyler","Bailey","Melanie","Bethany","Gerardo","Camille","Rachael","Sunny","Susan","Susie","April","Samantha","Jordan","Priscilla","Amanda","Margaret","Nadia","Danielle","Virginia","Eileen","Emanuel","Naomi","Nancy","Angelo","Angela","Sharon","Sierra","Darrell","Will","Lorraine","Kristin","Kristen","Esther","Estela","Brianna","Donald","Freddy","Ariel","Juana","Wendy","Julio","Julia","Julie","Brittany","Crystal","Victoria","Jocelyn","Brittney","Mackenzie","Harold","Bradley","Ismael","Lourdes","Liliana","Monique","Kendall","Lorenzo","Glenda","Felix","Raquel","Brooke","Irene","Bridget","Claudia","Irina","Jasmine","Lilian","Rebeca","Ruben","Mercedes","Catherine","Wesley","Zack","Zach","Junior","Marcelo","Marcela","Hector","Isaac","Pierre","Zachary","Julian","Shirley","Audrey","Sebastian","Holly","Nelly","Gabriel","Krystal","Nicolas","Malik","Byron","Manny","Mandy","Deborah","Griselda","Marty","Marta","Sydney","Mason","Marco","Mario","Marie","Maria","Mateo","Enrique","María","Mayra","Amelia","Prince","Gonzalo","Reynaldo","Chelsea","Israel","Teresa","Glenn","Norman","Eddie","Ofelia","Edgar","Edith","Jessie","Lillian","Maureen","Armando","Albert","Curtis","Maddie","Katelyn","Edwin","Josephine","Rhonda","Fidel","Kimberly","Tracey","Jeremy","Shelby","Shelly","Vanessa","Stephen","Lizbeth","Sheila","Jerome","Benjamin","Adrienne","Daisy","Rosario","Sherri","Sherry","Fernanda","Fernando","George","Travis","Celeste","Garrett","Dennis","Danny","Melinda","Denise","Ricardo","Clayton","David","Silvia","Pauline","Lawrence","Paulina","Melissa","Sandra","Natalie","Natalia","Jonathan","Gustavo","Natasha","Santos","Rebecca","Dorothy","Nikki","Megan","Nelson","Timothy","Octavio","Maurice","Cristian","Cristina","Jackie","German","Roland","Yasmin","Evelyn","Lance","Larry","Marisela","Laura","Destiny","Alexis","Gerald","Samuel","Maggie","Maribel","Isabella","Mariana","Kelsey","Kelvin","Pamela","Vicki","Vicky","Marilyn","Esmeralda","Kathryn","Mariela","Roberta","Roberto","Marissa","Marisol","Vilma","Vince","Yvette","Maritza","Princess","Esteban","Tammy","Alfred","Margarita","Tania","Madeline","Tanya","Regina","Elaine","Simone","Anthony","Tasha","Daniela","Cheryl","Ronald","Debra","Trevor","Derrick","Ronnie","Delia","Grace","Denis","Grant","Caleb","Derek","Justin","Miguel","Candy","Devin","Devon","Casey","Carol","Carly","Carla","Cathy","Valentina","Calvin","Philip","Alison","Angelica","Angelina","Brandon","Kenneth","Micah","Matthew","Johanna","Tricia","Alicia","Gloria","Mitchell","Marlene","Mikey","Mindy","Katherine","Diamond","Mirna","Charlotte","Mitch","Dianne","Misty","Missy","Jillian","Lauren","Laurie","Rodolfo","Leroy","Sylvia","Karla","Juanita","Karen","Kathy","Katie","Kendra","Kayla","Camila","Agustin","Bernard","Elena","Richard","Oscar","Eliza","Elisa","Eugene","Elias","Abby","Abel","Ellen","Adan","Adam","Elmer","Ellie","Aida","Noemi","William","Milton","Alan","Alba","Alex","Alec","Alma","Ally","Andy","Elvis","Elvia","Anne","Anna","Vivian","Sammy","Norma","Manuel","Terri","Sally","Terry","Colleen","Sandy","Willie","Sarah","Diana","Diane","Guadalupe","Sasha","Emily","Barb","Nadine","Guillermo","Diego","Beto","Beth","Bill","Gabrielle","Gabriella","Celia","Brad","Emilio","Franklin","Frank","Bonnie","Fredy","Nestor","Walter","Rodrigo","Cesar","Cassandra","Barry","Cecilia","Shelley","Carl","Blanca","Valerie","Valeria"]
+let arrayOfLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let DoReplaceTempString = "segnsidufgnsiuhrgkshsuhsdfjghsd"
+let DoNotReplaceTempString = "nftykhjbshktgsuhrtiguygerfkste"
+let name = "David";
+// acctually doin stuff
+getAllTextNodes().forEach((node) => {
+    const {nodeValue} = node;
+    let newValue = nodeValue;
+
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        newValue = newValue.replace(arrayOfNames[i] + " " , name + " ");
+        newValue = newValue.replace(arrayOfNames[i] , name);
+        //replaceIfNotProcededByLetter(newValue, arrayOfNames[i], name);
+        //newValue = replaceWordWithSpecialChar(newValue, arrayOfNames[i], name);
+    }
+/*
+    for (let i = 0; i < arrayOfNames.length; i++) {
+
+        newValue = replaceIfNotPrecededByLetter(newValue, arrayOfNames[i], name);
+    }
+
+
+ */
+
+    /*
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        let occurrences = occurrencesFunc(newValue, arrayOfNames[i], false);
+        for (let j = 0; j < occurrences; j++) {
+            let hasLetterAfter = false;
+
+            for (let k = 0; k < arrayOfLetters.length; k++) {
+                console.log(newValue.toString())
+                if (newValue.length > newValue.lastIndexOf(arrayOfNames[i]) + arrayOfNames[i].length) {
+                    if (newValue.at(newValue.lastIndexOf(arrayOfNames[i]) + arrayOfNames[i].length) == arrayOfLetters[k]) {
+                        hasLetterAfter = true;
+
+                    }
+                }
+            }
+            if (hasLetterAfter) {
+                newValue = replaceSubstringAtIndex(newValue,DoNotReplaceTempString,newValue.lastIndexOf(arrayOfNames[i]),newValue.lastIndexOf(arrayOfNames[i]) + arrayOfNames[i].length)
+            } else {
+                newValue = replaceSubstringAtIndex(newValue, DoReplaceTempString, newValue.lastIndexOf(arrayOfNames[i]), newValue.lastIndexOf(arrayOfNames[i]) + arrayOfNames[i].length)
+            }
+        }
+        newValue = newValue.replace(DoNotReplaceTempString,arrayOfNames[i] );
+        newValue = newValue.replace(DoReplaceTempString,name );
+
+    }
+     */
+
+
+    if (newValue !== nodeValue && newValue !== null) {
+        node.nodeValue = newValue;
+    }
+
+
+});
+
+
+function splitMethodReplacement(str, search, replace) {
+    let wordArray = str.split(" ");
+    for (let i = 0; i < wordArray.length; i++) {
+        if (wordArray[i] == search) {
+            wordArray[i] = replace;
+        }
+    }
+    let newString = "";
+    for (let i = 0; i < wordArray.length; i++) {
+        newString += wordArray[i] + " ";
+
+    }
+    return newString;
+}
+
+function replaceWordWithSpecialChar(str, search, replace) {
+    // Create a regular expression to match the search word followed by a special character
+    const regex = new RegExp(`\\b${search}\\b(?=[\\s.,!?;:'"()\\[\\]{}<>\0])`, 'g');
+    // Replace all occurrences of the search word followed by a special character with the replace word
+    return str.replace(regex, replace);
+}
+function replaceIfNotProcededByLetter(str, search, replace) {
+    const regex = new RegExp(`${search}(?![a-z])`, 'g');
+    return str.replace(regex, replace);
+}
+function replaceSubstringAtIndex(string, replacement,index,endIndex) {
+    let newString = string;
+
+       newString = newString.substring(0, index) + replacement + newString.substring(endIndex );
+       return newString;
+
+}
+function checkIfNameIsSolo(name, text) {
+    let tempString = "tewrg"
+    tempString.indexOf(name);
+
+
+}
+
+function getAllTextNodes() {
+    var walker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        null
+    );
+
+    var node;
+    var textNodes = [];
+
+    while (node = walker.nextNode()) {
+        textNodes.push(node);
+    }
+    return textNodes;
+
+}
+
+function occurrencesFunc(string, subString, allowOverlapping) {
+
+    string += "";
+    subString += "";
+    if (subString.length <= 0) return (string.length + 1);
+
+    var n = 0,
+        pos = 0,
+        step = allowOverlapping ? 1 : subString.length;
+
+    while (true) {
+        pos = string.indexOf(subString, pos);
+        if (pos >= 0) {
+            ++n;
+            pos += step;
+        } else break;
+    }
+    return n;
+}
